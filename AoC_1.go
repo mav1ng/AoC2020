aeca4f8d5a87804736cc5bee9927546517f13268
Viktor Spreng <m.spreng@stud.uni-heidelberg.de>
2020-12-05 21:57:38 +0100

    Solution AoC_1 part 2

--- AoC_1.go
+++ AoC_1.go
@@ -7,28 +7,43 @@ import "log"
 import "strings"
 import "strconv"
 
-func find_two(list []int) int {
-	sort.Ints(list)
-	fmt.Println(list)
+func find_two(list []int, num int) int {
 
-	i := 1
 	var low, high int = 0, len(list) - 1
-	for i < 10 {
-		fmt.Println(low, high)
-		fmt.Println(list[low] + list[high])
+	for low <= high {
+		// fmt.Println(low, high)
+		// fmt.Println(list[low] + list[high])
 		if low == high {
 			break
 			fmt.Println("There was no valid combination found!")
-		} else if list[low] + list[high] == 2020 {
+		} else if list[low] + list[high] == num {
 			return list[low] * list[high]
-		} else if list[low] + list[high] < 2020 {
+		} else if list[low] + list[high] < num {
 			low = low + 1
-		} else if list[low] + list[high] > 2020 {
+		} else if list[low] + list[high] > num {
 			high = high - 1
 		}
 	}
 
 	return 0	
+
+}
+
+func find_three(list []int) int {
+
+	var current_num, searched_num int
+	var ret int
+
+	for j:=0; j<len(list); j++ {
+		current_num = list[j]
+		searched_num = 2020 - current_num
+		ret = find_two(list, searched_num)
+		if ret != 0 {
+			return current_num * ret
+		}
+	}
+
+	return 0
 
 }
 
@@ -40,11 +55,11 @@ func main() {
 		log.Fatal(err)
 	}
 	
-	var t []string = strings.Split(string(content), "\n")
-	t = t[:len(t)-1]
-	fmt.Println(t)
-	var t2 = []int{}
 
+	var t []string = strings.Split(string(content), "\r\n")
+	t = t[:len(t)-1]
+	var t2 = []int{}
+	
 	for _, i := range t {
         	j, err := strconv.Atoi(i)
         	if err != nil {
@@ -54,10 +69,13 @@ func main() {
     	}
 
 
-	fmt.Println(t2)
-
 	list := []int{1721, 979, 366, 299, 675, 1456}
-	list = t2
 
-	fmt.Println(find_two(list))
+	sort.Ints(list)
+	sort.Ints(t2)
+
+	fmt.Println("Find Two", find_two(t2, 2020))
+	fmt.Println("Find Three:", find_three(t2))
+
+
 }
